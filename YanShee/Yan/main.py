10fa5164ac4fb594395eb29043ead193e5f70f0c
yangxiaoyang <848655901@qq.com>
2022-11-03 13:33:59 +0800

    2022-11-3 1.0 Yanshee机器人模拟赛题测试

--- YanShee/Yan/main.py
+++ YanShee/Yan/main.py
@@ -1,10 +1,10 @@
 import YanAPI as Yan
-import time as t
+import time
 
-Yan.yan_api_init("192.168.1.131")
-at_left = True
-x = 0
-box_num = None
+ip_addr = "192.168.1.141"
+Yan.yan_api_init(ip_addr)  # 初始化YanAPI
+at_left = True  # 二阶段位置
+box_num = '0'  # 保存物品放入位置
 
 
 def motion(name: str = "复位", direction: str = "", speed: str = "normal", repeat: int = 1, sy: bool = True) -> any:
@@ -29,14 +29,14 @@ def get_col_rec() -> str:
               "cyan": "青", "magenta": "洋红", "orange": "橙", "violet": "紫", "brown": "棕", "black": "黑", "white": "白",
               "gray": "灰"}
     Yan.start_color_recognition()
-    t.sleep(2.5)
-    col_rec = Yan.get_visual_task_result("color", "color_detect")["data"]["color"][0]["name"]
+    time.sleep(3)
     Yan.stop_color_recognition()
-    print("col_rec: %s" % col_rec)
+    col_res = Yan.get_visual_task_result("color", "color_detect")["data"]["color"][0]["name"]
+    print("col_res: %s" % col_res)
     for i in col_dt:
-        if i == col_rec:
-            tts("识别到" + col_dt[i] + "色物品")
+        if i == col_res:
             print("get_col_rec: %s" % col_dt[i])
+            tts("识别到" + col_dt[i] + "色物品")
             return col_dt[i]
     return "error"
 
@@ -51,14 +51,14 @@ def get_iat_col() -> str:
         for i in col_lt:
             idx = iat_res.find(i)
             if idx != -1:
-                print("get_iat_col: %s" % i)
-                tts(iat_res, False)
+                print("get_iat_col: %s " % i)
+                tts(iat_res)
                 return i
     return "error"
 
 
 def number1(left: bool = True):
-    global x
+    x = 0
     motion()
     while iat().find("开始") == -1:
         pass
@@ -66,65 +66,61 @@ def number1(left: bool = True):
         motion("左走", repeat=2)
         x -= 2
         motion()
-        motion("walk", "forward", repeat=7)
+        motion("walk", "forward", repeat=6)
+        motion()
         infrared = Yan.get_sensors_infrared_value() / 10.0
         if infrared < 5.5:
-            tts("识别到障碍物距离约为" + str(infrared) + "厘米", False)
+            tts("检测到障碍物距离约为" + str(infrared) + "厘米")
             motion("右走", repeat=4)
             x += 4
-        motion()
+            motion()
         motion("跨")
         motion("walk", "forward", repeat=7)
     else:
-        motion("右走", repeat=3)
-        x += 3
+        motion("右走", repeat=2)
+        x += 2
         motion()
-        motion("walk", "forward", repeat=7)
+        motion("walk", "forward", repeat=6)
+        motion()
         infrared = Yan.get_sensors_infrared_value() / 10.0
         if infrared < 5.5:
-            tts("识别到障碍物距离约为" + str(infrared) + "厘米", False)
+            tts("检测到障碍物距离约为" + str(infrared) + "厘米")
             motion("左走", repeat=4)
             x -= 4
-        motion()
+            motion()
         motion("跨")
-        motion("walk", "forward", repeat=9)
-    motion()
-    if x > 0:
-        # motion("左走", repeat=x - x)
-        pass
-    elif x < 0:
+        motion("walk", "forward", repeat=7)
+    print(x)
+    if x < 0:  # left
         motion("右走", repeat=-x)
-    x = 0
-    motion("walk", direction="forward", repeat=1)
+    elif x > 0:  # right
+        motion("左走", repeat=1)
     tts("已到达指定位置")
 
 
 def number2():
+    global at_left
+    global box_num
     motion()
-
     iat_col = get_iat_col()
     motion("识别")
     col_rec = get_col_rec()
-    motion("复位")
+    motion()
     if iat_col == col_rec:
         motion("抱")
     else:
-        global at_left
         at_left = False
         motion("walk", "backward", repeat=2)
         motion("后抱")
         get_col_rec()
     motion("抱复位")
-    global box_num
     box_num = Yan.sync_do_QR_code_recognition(-1)["data"]["contents"][0]
-    print("box_num: %s" % box_num)
-    motion("右走", repeat=7)
+    motion("右走", repeat=6)
 
 
 def number3():
-    qr = Yan.sync_do_QR_code_recognition(10)["data"]["contents"][0]
-    print("qr: %s" % qr)
-    if qr == box_num:
+    qr_result = Yan.sync_do_QR_code_recognition(-1)["data"]["contents"][0]
+    if qr_result == box_num:
         if not at_left:
             motion("walk", "forward", repeat=2)
         motion("放")
@@ -132,15 +128,17 @@ def number3():
         if at_left:
             motion("walk", "backward", repeat=2)
         motion("右看")
-        qr = Yan.sync_do_QR_code_recognition(10)["data"]["contents"][0]
-        print("qr: %s" % qr)
-        if qr == box_num:
+        qr_result = Yan.sync_do_QR_code_recognition(10)["data"]["contents"][0]
+        if qr_result == box_num:
             motion("右放")
         else:
             motion("后放")
+    tts("物品搬运完成", False)
+    motion()
 
 
 if __name__ == "__main__":
     number1(True)
     number2()
     number3()
+    pass
